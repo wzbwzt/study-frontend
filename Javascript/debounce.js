755318cb2e37d32ba9eff01745eb4dfc4d6a9483
wuzibing <wuzb@geovis.com.cn>
2024-07-22 18:20:19 +0800

    fix

---
+++ Javascript/debounce.js
@@ -0,0 +1,20 @@
+//去抖动
+
+const exec = (i) => {
+  console.log(i);
+};
+
+function debounce(fn, delay) {
+  let timeid;
+  return function (...args) {
+    clearTimeout(timeid);
+    timeid = setTimeout(() => {
+      fn(args);
+    }, delay);
+  };
+}
+
+const df = debounce(exec, 1000);
+for (let i = 0; i <= 10; i++) {
+  df(i);
+}
